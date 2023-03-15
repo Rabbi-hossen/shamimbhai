dcbfd6a06a9ea76debfa8b5f3cb4f4b39610dfba
RABBI HOSSEN <73291723+Rabbi-hossen@users.noreply.github.com>
2023-03-15 12:21:40 +0600

    Add files via upload

---
+++ r/signin.php
@@ -0,0 +1,38 @@
+<html>
+
+<body>
+    <fieldset>
+        <legend>
+            Signin
+        </legend>
+        <table>
+
+            <tr>
+                <td>Email</td>
+                <td>: <input type="email" name="email" value=""></td>
+            </tr>
+            <tr>
+                <td>password</td>
+                <td> : <input type="password" name="password" value=""> </td>
+            </tr>
+
+
+
+            <tr>
+
+                <td>
+                    <input type="submit" name="Signin" value="submit">
+                    <a href="sign_up">sign up</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="Forgot password?"> Forgot password? </a>
+                </td>
+            </tr>
+        </table>
+
+    </fieldset>
+</body>
+
+</html>
---
+++ r/signup.php
@@ -0,0 +1,46 @@
+<html>
+    <body>
+   <fieldset>
+    <legend>
+        Signup
+        </legend>
+   <table>
+    <tr> 
+        <td>Name </td>
+        <td>: <input type="text" name="name" value=""> </td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>: <input type="email" name="email" value="" ></td>
+    </tr>
+    <tr>
+        <td>password</td>
+        <td> : <input type="password" name="password" value=""> </td>
+    </tr>
+    <tr>
+        <td>re-password</td>
+        <td> : <input type="password" name="re-password" value=""> </td>
+    </tr>
+    <tr>
+        <td>
+            User type
+        </td>
+        <td >
+        
+        <input type="radio" name="user" value="user">user
+        <input type="radio" name="miner" value="miner">miner
+
+        </td>
+    </tr>
+    <tr>
+
+                                <td>
+                                    <input type="submit" name="Signup" value="submit">
+                                    <a href="signin">sign in</a>
+                                </td>
+                            </tr>
+   </table>
+
+   </fieldset>
+    </body>
+</html>
